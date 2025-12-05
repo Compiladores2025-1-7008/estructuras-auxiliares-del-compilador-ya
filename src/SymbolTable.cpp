d29dbcfcffb21c8662d89f5f6df6a09085b8de81
Aldo Jurado <emilio.jurado@ciencias.unam.mx>
2025-12-05 11:33:00 -0600

    chore: estructura inicial para tabla de símbolos

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,1 +1,38 @@
 #include "SymbolTable.hpp"
+#include <iostream>
+
+// Insertar un nuevo símbolo en la tabla
+bool SymbolTable::insert(const SymbolEntry &entry)
+{
+    return false;
+}
+
+// Obtener tipo por id
+int SymbolTable::getType(const std::string &id)
+{
+    return 0;
+}
+
+// Obtener dirección por id
+int SymbolTable::getAddress(const std::string &id)
+{
+    return 0;
+}
+
+// Obtener categoría por id
+Category SymbolTable::getCategory(const std::string &id)
+{
+    return Category::VAR;
+}
+
+// Obtener lista de parámetros por id
+std::vector<int> SymbolTable::getParams(const std::string &id)
+{
+    return {};
+}
+
+// Imprimir la tabla para depuración
+void SymbolTable::print() const
+{
+    return;
+}
--- src/SymbolTable.hpp
+++ src/SymbolTable.hpp
@@ -4,15 +4,18 @@
 #include <vector>
 #include <optional>
 
-enum class Category {
+enum class Category
+{
     VAR,
     CONST,
     STRUCT,
     FUNCTION,
     PARAM
 };
-
-struct SymbolEntry {
+// Recordatorio de cómo se ve la tabla de símbolos:
+// id | tipo | categoría | dirección | lista de parámetros
+struct SymbolEntry
+{
     std::string id;
     int typeId;
     Category category;
@@ -20,27 +23,35 @@ struct SymbolEntry {
     std::vector<int> params;
 };
 
-class SymbolTable {
+class SymbolTable
+{
 private:
     std::unordered_map<std::string, SymbolEntry> table;
 
 public:
+    // insert va a regresar regresa false si ya existía el id, true si se insertó correctamente
     bool insert(const SymbolEntry &entry);
+
     // -----------------------------------------
     // Consultas individuales simples
     // -----------------------------------------
+    // Devuelve el tipo asociado al id
     int getType(const std::string &id);
 
-    int getAddress(const std::string &id) ;
+    // Devuelve la dirección asociada al id
+    int getAddress(const std::string &id);
 
+    // Devuelve la categoría asociada al id
     Category getCategory(const std::string &id);
 
+    // Devuelve la lista de parámetros asociada al id
     std::vector<int> getParams(const std::string &id);
 
     // -----------------------------------------
     // Consulta completa (si necesitas todos los datos)
     // -----------------------------------------
-    const SymbolEntry* lookup(const std::string &id) const {
+    const SymbolEntry *lookup(const std::string &id) const
+    {
         auto it = table.find(id);
         return (it != table.end()) ? &it->second : nullptr;
     }
@@ -48,4 +59,3 @@ public:
     // Para imprimir/depurar
     void print() const;
 };
-
